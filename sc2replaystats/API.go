3537f148216a4bfe187c36a5dbba66f2ed16a97f
Damon Blais <damon.blais@gmail.com>
2020-11-10 14:43:22 -0800

    fix: APIRoot and WebRoot were swapped

--- sc2replaystats/API.go
+++ sc2replaystats/API.go
@@ -13,10 +13,10 @@ var (
 	Protocol = "https"
 
 	// APIRoot represents the base URL for requests to the sc2replaystats JSON-ish API
-	APIRoot = fmt.Sprintf("%s://%s", Protocol, Hostname)
+	APIRoot = fmt.Sprintf("%s://api.%s", Protocol, Hostname)
 
 	// WebRoot represents the base URL for requests to the sc2replaystats Website
-	WebRoot = fmt.Sprintf("%s://api.%s", Protocol, Hostname)
+	WebRoot = fmt.Sprintf("%s://%s", Protocol, Hostname)
 
 	// ClientIdentifier represents the "upload_method" shown to sc2replaystats
 	ClientIdentifier = fmt.Sprintf("sc2-rsu-%s", runtime.GOOS)
