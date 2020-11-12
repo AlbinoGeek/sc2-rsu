c98a56fee695109d3c5d010ffd57bfb5f4a07b96
Damon Blais <albinogeek@gmail.com>
2020-11-12 00:59:03 -0800

    doc: spelling

--- utils/SplitFilepath.go
+++ utils/SplitFilepath.go
@@ -3,7 +3,7 @@ package utils
 import "path/filepath"
 
 // SplitFilepath takes a relative or absolute path to a file, and returns the
-// separated path, filename, and file extention. If an extention is present,
+// separated path, filename, and file extension. If an extension is present,
 // it will start with a dot character, otherwise it will be an empty string.
 func SplitFilepath(name string) (fpath, fname, ext string) {
 	fpath = filepath.Dir(name)
