38b2aaad7409e8004e2f2e9ee940c1b4f4c4afcf
Damon Blais <albinogeek@gmail.com>
2020-11-13 00:20:44 -0800

    cleanup: code smells

--- sc2replaystats/ValidAPIKey.go
+++ sc2replaystats/ValidAPIKey.go
@@ -13,25 +13,17 @@ var ts2010 = time.Unix(1262304000, 0)
 func ValidAPIKey(key string) bool {
 	parts := strings.Split(key, ";")
 
-	// there are exactly thee parts
-	if len(parts) != 3 {
-		return false
-	}
-
-	// the first two parts are each 40 characters
-	if len(parts[0]) != 40 ||
+	// there are exectly three parts, and the first two are 40 characters each
+	if len(parts) != 3 ||
+		len(parts[0]) != 40 ||
 		len(parts[1]) != 40 {
 		return false
 	}
 
-	// the final part is a valid unix time
+	// the final part is a valid unix time after 2010
 	ts, err := strconv.ParseInt(parts[2], 10, 32)
-	if err != nil {
-		return false
-	}
-
-	// time must be after 2010 (sanity check)
-	if time.Unix(ts, 0).Before(ts2010) {
+	if err != nil ||
+		time.Unix(ts, 0).Before(ts2010) {
 		return false
 	}
 
