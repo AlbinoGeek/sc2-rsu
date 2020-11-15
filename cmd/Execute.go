915b3fab3cb7962ea5d416d91aa8862ff4efeaad
Damon Blais <albinogeek@gmail.com>
2020-11-14 18:49:21 -0800

    cleanup: cmd.Execute in own file

---
+++ cmd/Execute.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// Execute executes the root command once Initialize has been called
+func Execute() error {
+	defaultCfgFile = fmt.Sprintf("%s.%s", PROGRAM, cfgExt)
+
+	rootCmd.Use = PROGRAM
+	rootCmd.Version = fmt.Sprintf("%s, version %s-(%s-%s)", PROGRAM, VERSION, runtime.GOARCH, runtime.GOOS)
+
+	// ? should not require RAW mode just go get the dimensions...
+	if w, _, err := terminal.GetSize(0); err == nil {
+		termWidth = w
+	}
+
+	// Load Configuration on Initialize
+	defaultConfig()
+	cobra.OnInitialize(loadConfig)
+
+	// Attach Flags
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", fmt.Sprintf("config file (default is $HOME/%s)", defaultCfgFile))
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "enable debug logging for troubleshooting sake")
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+
+	// Add Commands
+	rootCmd.AddCommand(loginCmd)
+	rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(uploadCmd)
+	rootCmd.AddCommand(versionCmd)
+
+	return rootCmd.Execute()
+}
--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
@@ -16,7 +15,6 @@ import (
 	"github.com/fsnotify/fsnotify"
 	"github.com/kataras/golog"
 	"github.com/mitchellh/go-wordwrap"
-	"golang.org/x/crypto/ssh/terminal"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -259,34 +257,4 @@ func watchReplayStatus(rqid string) {
 
 		golog.Debugf("sc2replaystats process..: [%v] %s", rqid, rid)
 	}
-}
-
-// Execute executes the root command once Initialize has been called
-func Execute() error {
-	defaultCfgFile = fmt.Sprintf("%s.%s", PROGRAM, cfgExt)
-
-	rootCmd.Use = PROGRAM
-	rootCmd.Version = fmt.Sprintf("%s, version %s-(%s-%s)", PROGRAM, VERSION, runtime.GOARCH, runtime.GOOS)
-
-	// ? should not require RAW mode just go get the dimensions...
-	if w, _, err := terminal.GetSize(0); err == nil {
-		termWidth = w
-	}
-
-	// Load Configuration on Initialize
-	defaultConfig()
-	cobra.OnInitialize(loadConfig)
-
-	// Attach Flags
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", fmt.Sprintf("config file (default is $HOME/%s)", defaultCfgFile))
-	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "enable debug logging for troubleshooting sake")
-	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
-
-	// Add Commands
-	rootCmd.AddCommand(loginCmd)
-	rootCmd.AddCommand(updateCmd)
-	rootCmd.AddCommand(uploadCmd)
-	rootCmd.AddCommand(versionCmd)
-
-	return rootCmd.Execute()
 }
