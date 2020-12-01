72e403cda84db151814bd63b1b214ea5f3fd1c73
Damon Blais <albinogeek@gmail.com>
2020-12-01 03:41:05 -0800

    doc: partial

--- cmd/gui/fynewidget/Pane.go
+++ cmd/gui/fynewidget/Pane.go
@@ -7,6 +7,7 @@ import (
 	"github.com/AlbinoGeek/sc2-rsu/cmd/gui"
 )
 
+// Pane ...
 type Pane interface {
 	GetWindow() gui.Window
 
@@ -23,10 +24,13 @@ type Pane interface {
 	SetTitle(string)
 }
 
+// NewPane returns a pane to be used with navigation, specifying a title
 func NewPane(title string, window gui.Window) Pane {
 	return NewPaneWithIcon(title, nil, window)
 }
 
+// NewPaneWithIcon returns a pane to be used with navigation, specifying
+// title and icon
 func NewPaneWithIcon(title string, icon fyne.Resource, window gui.Window) Pane {
 	pane := &PaneBase{
 		icon:   icon,
@@ -46,6 +50,7 @@ func newNavigationLabel(title string, icon fyne.Resource) fyne.CanvasObject {
 	return b
 }
 
+// PaneBase implements Pane
 type PaneBase struct {
 	content fyne.CanvasObject
 	icon    fyne.Resource
@@ -54,35 +59,43 @@ type PaneBase struct {
 	window  gui.Window
 }
 
+// GetWindow returns the parent window this pane is shown in
 func (p *PaneBase) GetWindow() gui.Window {
 	return p.window
 }
 
+// GetContent returns the content of this pane
 func (p *PaneBase) GetContent() fyne.CanvasObject {
 	return p.content
 }
 
+// SetContent changes the element to be shown when this pane is selected
 func (p *PaneBase) SetContent(content fyne.CanvasObject) {
 	p.content = content
 }
 
+// GetLabel returns the assemebled label element for this pane
 func (p *PaneBase) GetLabel() fyne.CanvasObject {
 	return p.label
 }
 
+// GetIcon returns the icon shown in labels associated with this pane
 func (p *PaneBase) GetIcon() fyne.Resource {
 	return p.icon
 }
 
+// SetIcon changes the icon shown in labels associated with this pane
 func (p *PaneBase) SetIcon(icon fyne.Resource) {
 	p.icon = icon
 	p.label.(*widget.Button).SetIcon(icon)
 }
 
+// GetTitle returns the text shown in labels associated with this pane
 func (p *PaneBase) GetTitle() string {
 	return p.title
 }
 
+// SetTitle changes the text shown in labels associated with this pane
 func (p *PaneBase) SetTitle(title string) {
 	p.title = title
 	p.label.(*widget.Button).SetText(title)
